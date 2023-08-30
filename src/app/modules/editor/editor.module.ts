823c9e0ee624e03ad48f5a836d6ba8b69d2ce785
Veal Wang <49362109@qq.com>
2023-08-30 22:15:04 +0800

    修复css bug

--- src/app/modules/editor/editor.module.ts
+++ src/app/modules/editor/editor.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { EditorMainAreaComponent } from './components/editor-main-area/editor-main-area.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [EditorMainAreaComponent],
-  imports: [CommonModule, DndModule, MatCardModule, MatSlideToggleModule],
+  imports: [CommonModule, DndModule, FlexLayoutModule, MatCardModule, MatSlideToggleModule],
   exports: [EditorMainAreaComponent],
 })
 export class EditorModule {}
